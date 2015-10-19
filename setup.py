52e6bf92874c103b0be5e0805df9a40991a74c1a
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2015-10-19 08:32:19 +0000

    * typhon/__init__.py: Define version number here. Set to 0.0.4.
    Do not increase version number on commit! It will only be increased
    after a release has been upload to pypi.
    
    * setup.py, doc/conf.py: Use __version__ from init file.
    
    * doc/conf.py: Use RTD theme.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9507 aaf1aab0-4228-0410-ad68-8dceda47f409

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.3',
+    from typhon import __version__
+    version=__version__,
 
     description='Tools for atmospheric research',
     long_description=long_description,
--- typhon/__init__.py
+++ typhon/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 
+__version__ = '0.0.4'
+
