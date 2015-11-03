5171fe7b9c6aee88a87e22c95a2c8886f625949c
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2015-11-03 12:34:47 +0000

    * typhon/__init__.py (test):
    Fix bug that broke function if invoked in other directories than typhon/.
    The needed path is now determined using the __file__ variable.
    
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9543 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/__init__.py
+++ typhon/__init__.py
@@ -8,7 +8,9 @@ from . import files
 
 def _runtest():
     """Run all tests."""
+    from os.path import dirname
     import nose
-    nose.run()
+    loader = nose.loader.TestLoader(workingDir=dirname(__file__))
+    return nose.run(testLoader=loader)
 
 test = _runtest
