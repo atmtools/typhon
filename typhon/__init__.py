9f0066b45c8c7733d680398159f0deaac3cfd39a
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2015-11-03 09:04:54 +0000

    * typhon/arts/tests/reference:
    Added directory which includes several test XML files.
    
    * typhon/arts/tests/test_matpack_types.py:
    
    Added module which provides basic test functions for reading XML files.
    
    * typhon/__init__.py: Added test() to run all tests.
    
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9542 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/__init__.py
+++ typhon/__init__.py
@@ -5,3 +5,10 @@ __version__ = '0.0.4'
 from . import arts
 from . import files
 
+
+def _runtest():
+    """Run all tests."""
+    import nose
+    nose.run()
+
+test = _runtest
---
+++ typhon/arts/tests/test_matpack_types.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""Testing the basic ARATS XML functions
+
+This module provides basic functions to test the reading and writing
+of ARTS XML files.
+"""
+
+import os
+import numpy as np
+
+from nose.tools import with_setup
+
+from typhon.arts import xml
+
+
+# set path to reference directory as global variable
+ref_dir = os.path.join(os.path.dirname(__file__), "reference", "")
+
+def test_load_index():
+    """Load reference XML file for ARTS type Index."""
+    assert xml.load(ref_dir + 'index.xml') == 0
+
+def test_load_array():
+    """Load reference XML file for ARTS type Array."""
+    reference = _create_tensor(1)
+    test_data = xml.load(ref_dir + 'array.xml')
+    assert np.array_equal(test_data, reference)
+
+def test_load_matrix():
+    """Load reference XML file for ARTS type Matrix."""
+    reference = _create_tensor(2)
+    test_data = xml.load(ref_dir + 'matrix.xml')
+    assert np.array_equal(test_data, reference)
+
+def test_load_tensor():
+    """Load reference XML files for different tensor types."""
+    for n in range(3, 8):
+        yield _load_tensor, n
+
+def _load_tensor(n):
+    """Load tensor of dimension n and compare data to reference.
+
+    Args:
+        n (int): number of dimensions
+
+    """
+    reference = _create_tensor(n)
+    test_data = xml.load(ref_dir + 'tensor{}.xml'.format(n))
+    assert np.array_equal(test_data, reference)
+
+def _create_tensor(n):
+    """Create a tensor of dimension n.
+
+    Create a tensor with n dimensions with two entries in each dimension.
+    The tensor is filled with increasing integers starting with 0.
+
+    Args:
+        n (int): number of dimensions
+
+    Returns:
+        np.ndarray: n-dimensional tensor
+
+    """
+    return np.arange(2**n).reshape(2*np.ones(n).astype(int))
+
