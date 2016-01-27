63f1b8d3f4800aee22f93ab7512d41fc2f325e87
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-01-27 08:24:20 +0000

    * typhon/arts/xml/tests/load_arts_xml_data.py:
    Now working with ARTS_DATA_PATH variables containing several separated
    directories. Every directory stored in ARTS_DATA_PATH is searched for XML
    files recursively.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9664 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/arts/xml/tests/load_arts_xml_data.py
+++ typhon/arts/xml/tests/load_arts_xml_data.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 
 import os
-import fnmatch
 
 from typhon.arts import xml
 
@@ -13,12 +12,14 @@ from typhon.arts import xml
 #
 # Notes:
 #   This is not a docstring to ensure readble output in nosetests.
+data_path = os.getenv('ARTS_DATA_PATH')
 def test_load_arts_xml_data():
-    if os.environ.get('ARTS_DATA_PATH'):
-        for root, _, filenames in os.walk(os.environ.get('ARTS_DATA_PATH')):
-            for filename in filenames:
-                if filename.endswith(('.xml', '.xml.gz')):
-                    yield _load_xml, os.path.join(root, filename)
+    if data_path:
+        for d in data_path.split(os.path.pathsep):
+            for root, _, filenames in os.walk(d):
+                for filename in filenames:
+                    if filename.endswith(('.xml', '.xml.gz')):
+                        yield _load_xml, os.path.join(root, filename)
     else:
         raise Exception('ARTS_DATA_PATH is not set.')
 
