543ca543824eb935fae52783a11139af7a99c7d0
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-02-11 08:52:00 +0000

    * typhon/cm/__init__.py:
    Colormaps can now be generated through dictionaries containing red, blue and
    green entries (see matplotlib's LinearSegmentedColormap).
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9687 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/cm/__init__.py
+++ typhon/cm/__init__.py
@@ -13,10 +13,23 @@ from ._cmocean import datad as _cmocean_datad
 
 datad = _cmocean_datad
 
+def _rev_cdict(cdict):
+    """Revert a dictionary containing specs for a LinearSegmentedColormap."""
+    rev_cdict = {}
+    for k, v in cdict.items():
+        rev_cdict[k] = [(1.0 - x, y1, y0) for x, y0, y1 in reversed(v)]
+    return rev_cdict
+
 cmaps = {}
 for (name, data) in datad.items():
-    cmaps[name] = LinearSegmentedColormap.from_list(name, data)
-    cmaps[name + '_r'] =LinearSegmentedColormap.from_list(name + '_r', data[::-1])
+    if 'red' in data:
+        cmaps[name] = LinearSegmentedColormap(name, data)
+        cmaps[name + '_r'] = LinearSegmentedColormap(
+            name + '_r',_rev_cdict(data))
+    else:
+        cmaps[name] = LinearSegmentedColormap.from_list(name, data)
+        cmaps[name + '_r'] = LinearSegmentedColormap.from_list(
+            name + '_r', data[::-1])
 
 locals().update(cmaps)
 
