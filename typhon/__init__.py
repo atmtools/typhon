06d47a47ef337e798caa84880b9553cf9d8a0ef7
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-03-07 12:11:23 +0000

    * typhon/__init__.py:
    Fix runtime error when calling _runtest() from jupyter qtconsole and
    notebook. Bug reported by Lukas.
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9745 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/__init__.py
+++ typhon/__init__.py
@@ -17,9 +17,10 @@ from . import utils
 def _runtest():
     """Run all tests."""
     from os.path import dirname
+    from sys import argv
     import nose
     loader = nose.loader.TestLoader(workingDir=dirname(__file__))
-    return nose.run(testLoader=loader)
+    return nose.run(argv=[argv[0]], testLoader=loader)
 
 
 test = _runtest
