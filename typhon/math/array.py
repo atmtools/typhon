59ea4e7ac638d20935ee307081f4ca810b9ce18a
gerrit <gerrit@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-03-18 18:00:15 +0000

    2016-03-18  Gerrit Holl  <g.holl@reading.ac.uk>
    	* typhon/math/array.py(limit_ndarray):
    
    	- New function to select elements from structured ndarray based on
    	ranges.  Inspired by a similar function in atmlab.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9768 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/math/array.py
+++ typhon/math/array.py
@@ -30,3 +30,56 @@ def localmin(arr):
 
     return localmin
 
+def limit_ndarray(M, limits):
+    """Select elements from structured ndarray based on value ranges
+
+    This function filters a structured ndarray based on ranges defined for
+    zero or more fields.  For each field f with limits (lo, hi), it will
+    select only those elements where lo<=X[f]<hi.
+
+    >>> X = array([(2, 3), (4, 5), (8, 2), (5, 1)],
+                   dtype=[("A", "i4"), ("B", "i4")])
+
+    >>> print(limit_ndarray(X, {"A": (2, 5)}))
+    [(2, 3) (4, 5)]
+
+    >>> X = array([([2, 3], 3), ([4, 6], 5), ([8, 3], 2), ([5, 3], 1)],
+                   dtype=[("A", "i4", 2), ("B", "i4")])
+
+    >>> print(limit_ndarray(X, {"A": (2, 5, "all")}))
+    [([2, 3], 3)]
+
+    Arguments:
+        
+        M (numpy.ndarray): 1-D structured ndarray
+
+        limits (dict): Dictionary with limits.  Keys must correspond to
+            fields in M.  If this is a scalar field
+            (`M.dtype[field].shape==()`), values are tuples (lo, hi).
+            If this is a multidimensional field, values are tuples (lo,
+            hi, mode), where mode must be either `all` or `any`.
+            Values in the range [lo, hi) are retained, applying all or any
+            when needed.
+
+    Returns:
+
+        ndarray subset of M.  This is a view, not a copy.
+    """
+
+    selection = numpy.ones(shape=M.shape, dtype="?")
+
+    for (field, val) in limits.items():
+        ndim = len(M.dtype[field].shape)
+        if ndim == 0:
+            (lo, hi) = val
+            selection = selection & (M[field] >= lo) & (M[field] < hi)
+        else:
+            (lo, hi, mode) = val
+            lelo = M[field] >= lo
+            sthi = M[field] < hi
+            while lelo.ndim > 1:
+                lelo = getattr(lelo, mode)(-1)
+                sthi = getattr(sthi, mode)(-1)
+            selection = (selection & lelo & sthi)
+
+    return M[selection]
