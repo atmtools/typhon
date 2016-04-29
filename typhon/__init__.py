1b5f015470941571ac6eb6fec9ee107bea926d16
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-04-29 06:46:38 +0000

    * typhon/__init__.py: 0.2.10 Release.
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9817 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/__init__.py
+++ typhon/__init__.py
@@ -2,9 +2,9 @@
 
 
 def get_version_info():
-    VERSION = '0.2.9'
+    VERSION = '0.2.10'
     # Add revision number for development versions
-    DEVBUILD = True
+    DEVBUILD = False
 
     if DEVBUILD:
         from os import path
