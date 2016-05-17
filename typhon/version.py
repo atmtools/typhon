d51037ce4b8bd331797103dff2d7fc132b2533d7
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-05-17 07:08:23 +0000

    * typhon/version.py: 0.3.0 Release.
    * setup.py: Change dev status to '4 - Beta'.
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9844 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/version.py
+++ typhon/version.py
@@ -2,9 +2,9 @@
 
 
 def get_version_info():
-    VERSION = '0.2.16'
+    VERSION = '0.3.0'
     # Add revision number for development versions
-    DEVBUILD = True
+    DEVBUILD = False
 
     if DEVBUILD:
         from os import path
