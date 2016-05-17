e10b60073c0e4562184736ba0a5c92dfcc45045c
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-05-17 06:45:11 +0000

    * typhon/utils/__init.py__:
    Added function extract_block_diag() to extract blocks around the diagonal
    of a square matrix.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9843 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/utils/__init__.py
+++ typhon/utils/__init__.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 
 from . import latex
 from . import cache
 from . import metaclass
 
-__all__ = ["latex", "cache", "metaclass"]
+__all__ = ["latex",
+           "cache",
+           "metaclass",
+           "extract_block_diag",
+           ]
+
+
+def extract_block_diag(M, n):
+    """Extract diagonal blocks from square Matrix.
+
+    Args:
+        M (np.array): Square matrix.
+        n (int): Number of blocks to extract.
+
+    Example:
+        >>> foo = np.array([[ 1.,  1.,  0.,  0.],
+        ... [ 1.,  1.,  0.,  0.],
+        ... [ 0.,  0.,  2.,  2.],
+        ... [ 0.,  0.,  2.,  2.]])
+        >>> extract_block_diag(foo, 2)
+        [array([[ 1.,  1.],
+                [ 1.,  1.]]), array([[ 2.,  2.],
+                [ 2.,  2.]])]
+
+    """
+    return [np.split(m, n, axis=1)[i] for i, m in enumerate(np.split(M, n))]
