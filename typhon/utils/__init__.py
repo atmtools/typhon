f06b16d53a9e7e9cb2719e70d627c9ad8c3ef2ff
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-05-23 07:39:17 +0000

    * typhon/utils/__init__.py:
    Added class to provide a simple time profiling utility.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9855 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/utils/__init__.py
+++ typhon/utils/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import time
+
 import numpy as np
 
 from . import latex
@@ -9,6 +11,7 @@ __all__ = ["latex",
            "cache",
            "metaclass",
            "extract_block_diag",
+           "Timer",
            ]
 
 
@@ -31,3 +34,55 @@ def extract_block_diag(M, n):
 
     """
     return [np.split(m, n, axis=1)[i] for i, m in enumerate(np.split(M, n))]
+
+
+class Timer(object):
+    """Provide a simple time profiling utility.
+
+    Timer class adapted from blog entry [0].
+
+    [0] https://www.huyng.com/posts/python-performance-analysis
+
+    Parameters:
+        verbose: Print results after stopping the timer.
+
+    Examples:
+        Timer in with block:
+
+        >>> import time
+        >>> with Timer():
+        ...     time.sleep(1)
+        elapsed time: 1.001s
+
+        Timer object:
+
+        >>> import time
+        >>> r = Timer.start()
+        >>> time.sleep(1)
+        >>> t.stop()
+        >>> print(t.secs)
+        elapsed time: 1.001s
+
+    """
+    def __init__(self, verbose=True):
+        self.verbose = verbose
+
+    def __enter__(self):
+        return self.start()
+
+    def __exit__(self, *args):
+        self.stop()
+
+    def start(self):
+        """Start timer."""
+        self.starttime = time.time()
+        return self
+
+    def stop(self):
+        """Stop timer."""
+        self.endtime = time.time()
+        self.secs = self.endtime - self.starttime
+        self.msecs = self.secs * 1000
+        if self.verbose:
+            print('elapsed time: {:d}m{:.3f}s'.format(
+                int(self.secs//60), self.secs % 60))
