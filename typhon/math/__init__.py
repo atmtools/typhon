c54a3c6b12742f2c4269eaa674bd9544cff13409
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-08-28 16:12:12 +0000

    * typhon/math/__init__.py:
    Added a function to calculate column integrals.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9950 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/math/__init__.py
+++ typhon/math/__init__.py
@@ -1,7 +1,28 @@
 """Maths-related modules
 """
+import numpy as np
 
 from . import stats
 from . import array
 
-__all__ = ["stats", "array"]
+__all__ = ['stats',
+           'array',
+           'integrate_column',
+           ]
+
+
+def integrate_column(y, z, axis=None):
+    """Calculate the column integral of given data.
+
+    Parameters:
+        y (np.array): Data array.
+        z (np.array): Height levels.
+
+    Returns:
+        float: Column integral.
+
+    """
+    # TODO: Find a clean way to handle multidimensional input.
+    y_level = (y[1:] + y[:-1]) / 2
+
+    return np.nansum(y_level * np.diff(z), axis=axis)
