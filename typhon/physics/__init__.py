5106b737032ff5c237cf82f579c9741c1c65a432
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-09-15 14:03:45 +0000

    * typhon/physics/__init__.py:
    Ported Fresnel and Snell functions from atmlab.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@9990 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/physics/__init__.py
+++ typhon/physics/__init__.py
@@ -12,6 +12,8 @@ __all__ = ['units',
            'em',
            'planck',
            'planck_wavelength',
+           'snell',
+           'fresnel',
            ]
 
 
@@ -49,3 +51,118 @@ def planck_wavelength(l, T):
     k = constants.boltzmann
 
     return 2 * h * c**2 / (l**5 * (np.exp(h * c / (l * k * T)) - 1))
+
+
+def snell(n1, n2, theta1):
+    """Calculates the angle of the transmitted wave, according to Snell's law.
+
+    Snell's law for the case when both *n1* and *n2* have no imaginary part
+    is found in all physics handbooks.
+
+    The expression for complex *n2* is taken from "An introduction to
+    atmospheric radiation" by K.N. Liou (Sec. 5.4.1.3).
+
+    No expression that allows *n1* to be complex has been found.
+
+    If theta2 is found to be complex, it is returned as NaN. This can happen
+    when n1 > n2, and corresponds to a total reflection and there is no
+    transmitted part.
+
+    The refractive index and the dielectric constant, epsilon, are releated
+    as
+
+    .. math::
+        n = \sqrt{\epsilon}
+
+    Parameters:
+        n1 (float): Refractive index for medium of incoming radiation.
+        n2 (float): Refractive index for reflecting medium.
+        theta1 (float): Angle between surface normal
+            and incoming radiation [degree].
+
+    Returns:
+        float: Angle for transmitted part [degree].
+
+    """
+    __credits__ = 'Patrick Eriksson'
+
+    if np.any(np.real(n1) <= 0) or np.any(np.real(n2) <= 0):
+        raise Exception('The real part of *n1* and *n2* can not be <= 0.')
+
+    if np.all(np.isreal(n1)) and np.all(np.isreal(n2)):
+        theta2 = np.arcsin(n1 * np.sin(np.deg2rad(theta1)) / n2)
+
+    elif np.all(np.isreal(n1)):
+        mr2 = (np.real(n2) / n1)**2
+        mi2 = (np.imag(n2) / n1)**2
+        sin1 = np.sin(np.deg2rad(theta1))
+        s2 = sin1 * sin1
+        Nr = np.sqrt((mr2 - mi2 + s2
+                      + np.sqrt((mr2 - mi2 - s2)**2 + 4 * mr2 * mi2)
+                      ) / 2)
+        theta2 = np.arcsin(sin1 / Nr)
+    else:
+        raise Exception('No expression implemented for imaginary *n1*.')
+
+    if not np.all(np.isreal(theta2)):
+        theta2 = np.nan
+
+    return np.rad2deg(theta2)
+
+
+def fresnel(n1, n2, theta1):
+    r"""Fresnel formulas for surface reflection.
+
+    The amplitude reflection coefficients for a flat surface can also be
+    calculated (Rv and Rh). Note that these are the coefficients for the
+    amplitude of the wave. The power reflection coefficients are
+    obtained as
+
+    .. math::
+        r = \lvert R \rvert^2
+
+    The expressions used are taken from Eq. 3.31 in "Physical principles of
+    remote sensing", by W.G. Rees, with the simplification that that relative
+    magnetic permeability is 1 for both involved media. The theta2 angle is
+    taken from snell.m.
+
+    The refractive index of medium 2  (n2) can be complex. The refractive
+    index and the dielectric constant, epsilon, are releated as
+
+    .. math::
+        n = \sqrt{\epsilon}
+
+    No expression for theta2 that allows *n1* to be complex has been found.
+
+    If theta2 is found to be complex, it is returned as NaN. This can happen
+    when n1 > n2, and corresponds to a total reflection and there is no
+    transmitted part. Rv and Rh are here set to 1.
+
+    Parameters:
+        n1 (float): Refractive index for medium of incoming radiation.
+        n2 (float): Refractive index for reflecting medium.
+        theta1 (float): Angle between surface normal
+            and incoming radiation [degree].
+
+    Returns:
+        float, float, float:
+            Reflection coefficient for vertical polarisation,
+            reflection coefficient for horisontal polarisation,
+            angle for transmitted part [degree].
+
+    """
+    __credits__ = 'Patrick Eriksson'
+
+    if np.any(np.imag(n1) < 0) or np.any(np.imag(n2) < 0):
+        raise Exception(
+            'The imaginary part of *n1* and *n2* can not be negative.')
+
+    theta2 = snell(n1, n2, theta1)
+
+    costheta1 = np.cos(np.deg2rad(theta1))
+    costheta2 = np.cos(np.deg2rad(theta2))
+
+    Rv = (n2 * costheta1 - n1 * costheta2) / (n2 * costheta1 + n1 * costheta2)
+    Rh = (n1 * costheta1 - n2 * costheta2) / (n1 * costheta1 + n2 * costheta2)
+
+    return Rv, Rh, theta2
