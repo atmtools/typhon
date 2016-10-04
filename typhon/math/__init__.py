366f1dc3e743f9994495ba5089c225c93d765588
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-10-04 10:13:52 +0000

    * typhon/math/__init__.py (sum_digits):
    Function to calculate the sum of digits.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@10030 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/math/__init__.py
+++ typhon/math/__init__.py
@@ -8,6 +8,7 @@ from . import array
 __all__ = ['stats',
            'array',
            'integrate_column',
+           'sum_digits',
            ]
 
 
@@ -26,3 +27,25 @@ def integrate_column(y, z, axis=None):
     y_level = (y[1:] + y[:-1]) / 2
 
     return np.nansum(y_level * np.diff(z), axis=axis)
+
+
+def sum_digits(n):
+    """Calculate the sum of digits.
+
+    Parameters:
+       n (int): Number.
+
+    Returns:
+        int: Sum of digitis of n.
+
+    Examples:
+        >>> sum_digits(42)
+        6
+
+    """
+    s = 0
+    while n:
+        s += n % 10
+        n //= 10
+
+    return s
