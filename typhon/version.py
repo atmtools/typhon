ab78a22f4e05d2bd55f8717d1028b54fee978cfd
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-10-21 07:45:33 +0000

    * typhon/version.py: 0.3.4 Release.
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@10079 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/version.py
+++ typhon/version.py
@@ -2,9 +2,9 @@
 
 
 def get_version_info():
-    VERSION = '0.3.3'
+    VERSION = '0.3.4'
     # Add revision number for development versions
-    DEVBUILD = True
+    DEVBUILD = False
 
     if DEVBUILD:
         from os import path
