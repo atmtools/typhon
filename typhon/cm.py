6dbf435942840655558bd06bd5ff374c81477e9a
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-10-31 09:37:44 +0000

    * typhon/cm.py:
    Also import typhon.plots.colors to mimic the old structure completely.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@10094 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/cm.py
+++ typhon/cm.py
@@ -3,6 +3,7 @@
 """Simple deprecation warning."""
 from warnings import warn
 from typhon.plots.cm import *  # noqa
+from typhon.plots.colors import *  # noqa
 
 
 warn('Module has moved to typhon.plots.', DeprecationWarning, stacklevel=2)
