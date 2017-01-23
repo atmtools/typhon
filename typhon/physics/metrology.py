f74fcb5c386ae7a4aa9620171b7b586a0f5b4b99
gerrit <gerrit@aaf1aab0-4228-0410-ad68-8dceda47f409>
2017-01-23 17:05:35 +0000

    2017-01-23  Gerrit Holl  <g.holl@reading.ac.uk>
    	* typhon/physics/metrology.py:
    	
    	- We don't want uncertainties expressed in the free symbols if some
    	symbols are arrays; rather, each element of the array needs to have
    	its own uncertainty expression.  This is particularly important if
    	different elements of an array have different units, such as may be
    	the case when the coefficients of a polynomial are represented as an
    	array.  Added a helper function to use for this.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@10237 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/physics/metrology.py
+++ typhon/physics/metrology.py
@@ -1,20 +1,26 @@
 """Functions related to metrology
 
-This module has a soft dependency on sympy <http://www.sympy.org>
+All functions need sympy.
 """
 
-def express_uncertainty(expr):
+
+def express_uncertainty(expr, aliases={}):
     """For a sympy expression, calculate uncertainty.
 
     Uncertainty is given in the Guides to Uncertainties and Measurements
     (GUM), see http://www.bipm.org/en/publications/guides/gum.html
     equations 10 and 13.
 
+    This takes all free symbols.  If you have IndexedBase symbols, you may
+    want to pass them into aliases {free_symbol:
+    indexed_base_symbol[index]} is this is not identified automatically.
+
     Limitation: currently assumes all input quantities are uncorrelated!
 
     Arguments:
         
         expr (Expr): Expression for which to calculate uncertainty
+        aliases (Mapping): Mapping of replacements to apply prior.
 
     Returns:
         
@@ -24,6 +30,30 @@ def express_uncertainty(expr):
     import sympy
     u = sympy.Function("u")
     rv = sympy.sympify(0)
-    for sym in expr.free_symbols:
+    for sym in recursive_args(expr):
+        sym = aliases.get(sym, sym)
         rv += sympy.diff(expr, sym)**2 * u(sym)**2
     return sympy.sqrt(rv)
+
+def recursive_args(expr, stop_at=None):
+    """Get arguments for `expr`, stopping at certain types
+
+    Get all arguments in expression down to the levels in `expr`.  When
+    `expr` is only `{sympy.Symbol}` this is identical to
+    `expr.free_symbols`, but in some cases we want to retain IndexedBase,
+    for example, when evaluating uncertainies.
+
+    This is mainly a helper for express_uncertainty where we don't want to
+    descend beyond IndexedBase
+    """
+
+    import sympy
+    if stop_at is None:
+        stop_at = {sympy.Symbol, sympy.Indexed, sympy.Function}
+    args = set()
+    for arg in expr.args:
+        if any(isinstance(arg, tp) for tp in stop_at):
+            args.add(arg)
+        else:
+            args.update(recursive_args(arg, stop_at=stop_at))
+    return args
