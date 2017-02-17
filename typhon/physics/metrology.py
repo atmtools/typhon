9b0ffbc4c05078b51295b0609f69aa3999b27965
gerrit <gerrit@aaf1aab0-4228-0410-ad68-8dceda47f409>
2017-02-17 19:24:21 +0000

    2017-02-17  Gerrit Holl  <g.holl@reading.ac.uk>
    	* typhon/physics/metrology.py:
    	
    	(express_uncertainty): Add permissive mode where a single failed
    	derivative does not cause the entire expression to fail (awaiting a
    	fix for the failures).
    
    	(recursive_args): Bugfix, was dropping indexed values completely
    	rather than merely not descending into their index and indexbase.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@10304 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/physics/metrology.py
+++ typhon/physics/metrology.py
@@ -3,8 +3,9 @@
 All functions need sympy.
 """
 
+import warnings
 
-def express_uncertainty(expr, aliases={}):
+def express_uncertainty(expr, aliases={}, on_failure="raise"):
     """For a sympy expression, calculate uncertainty.
 
     Uncertainty is given in the Guides to Uncertainties and Measurements
@@ -21,6 +22,11 @@ def express_uncertainty(expr, aliases={}):
         
         expr (Expr): Expression for which to calculate uncertainty
         aliases (Mapping): Mapping of replacements to apply prior.
+        on_failure (str): Signals what to do when some variables cannot be
+            differentiated against.  This appears to be the case for
+            indexed quantities (see
+            https://github.com/sympy/sympy/issues/12191).  Default is
+            'raise', but can set to 'warn' instead.
 
     Returns:
         
@@ -32,7 +38,15 @@ def express_uncertainty(expr, aliases={}):
     rv = sympy.sympify(0)
     for sym in recursive_args(expr):
         sym = aliases.get(sym, sym)
-        rv += sympy.diff(expr, sym)**2 * u(sym)**2
+        try:
+            rv += sympy.diff(expr, sym)**2 * u(sym)**2
+        except ValueError as v:
+            if on_failure == "raise" or not "derivative" in v.args[0]:
+                raise
+            else:
+                warnings.warn("Unable to complete derivative on {!s}. "
+                    "IGNORING uncertainty on {!s}!  Derivative failed with: "
+                    "{:s}".format(expr, sym, v.args[0]))
     return sympy.sqrt(rv)
 
 def recursive_args(expr, stop_at=None, partial_at=None):
@@ -53,16 +67,25 @@ def recursive_args(expr, stop_at=None, partial_at=None):
         stop_at = (sympy.Symbol, sympy.Indexed)
     if partial_at is None:
         partial_at = sympy.concrete.expr_with_limits.ExprWithLimits
-    args = set()
     if isinstance(expr, stop_at):
-        return args
+        # to return {expr} here leads to infinite recursion!
+        return set()
     if isinstance(expr, partial_at):
         return recursive_args(expr.args[0])
+    args = set()
     for arg in expr.args:
         if isinstance(arg, stop_at):
             args.add(arg)
         elif isinstance(arg, partial_at):
-            args.update(recursive_args(arg.args[0], stop_at=stop_at))
+            # in the case arg.args[0] is a stop_at, recursive_args will
+            # return empty; but then we still need to add the expression a
+            # level higher up.  For example, support we have
+            # arg=Sum(T_PRT[n], (n, 0, N)), then arg.args[0]=T_PRT[n],
+            # recursive_args(arg.args[0]) = set() (if sympy.Indexed is in
+            # stop_at, such as by default), and neither gets added.
+            args.update((
+                {arg.args[0]} if isinstance(arg.args[0], stop_at) else set()) |
+                recursive_args(arg.args[0], stop_at=stop_at))
         else:
             args.update(recursive_args(arg, stop_at=stop_at))
     return args
