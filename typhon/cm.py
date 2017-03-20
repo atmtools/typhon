517d37e927e274b6bd770c5a8b3aaed8da87d78c
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2017-03-20 12:05:02 +0000

    * typhon/cm.py: Removed.
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@10367 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/cm.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Simple deprecation warning."""
-from warnings import warn
-from typhon.plots.cm import *  # noqa
-from typhon.plots.colors import *  # noqa
-
-
-warn('Module has moved to typhon.plots.', DeprecationWarning, stacklevel=2)
