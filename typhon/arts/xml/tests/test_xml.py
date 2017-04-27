8e714a90788eb57eaef3b8efd09ac8a25c7e1645
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2017-04-27 12:40:07 +0000

    * typhon/arts/xml/tests/test_xml.py:
    Fixed path to reference files.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@10421 aaf1aab0-4228-0410-ad68-8dceda47f409

--- typhon/arts/xml/tests/test_xml.py
+++ typhon/arts/xml/tests/test_xml.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Testing high-level functionality in typhon.arts.xml.
 """
+from os.path import (dirname, join)
+
 import numpy as np
 from nose.tools import raises
 
@@ -9,15 +11,17 @@ from typhon.arts import xml
 
 class TestXML(object):
     """Testing high-level functionality in typhon.arts.xml."""
+    ref_dir = join(dirname(__file__), "reference")
+
     def test_load_directory(self):
         """Test loading all XML files in a directory."""
-        t = xml.load_directory('reference/')
-        ref = xml.load('reference/vector.xml')
+        t = xml.load_directory(self.ref_dir)
+        ref = xml.load(join(self.ref_dir, 'vector.xml'))
 
         assert np.allclose(t['vector'], ref)
 
     @raises(KeyError)
     def test_load_directory_exclude(self):
         """Test excluding files when loading directory content."""
-        t = xml.load_directory('reference/', exclude=['vector.xml'])
+        t = xml.load_directory(self.ref_dir, exclude=['vector.xml'])
         t['vector']
