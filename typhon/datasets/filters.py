66504c87d95743ff670ff6481863bbc9b1243a18
gerrit <gerrit@aaf1aab0-4228-0410-ad68-8dceda47f409>
2017-07-28 08:48:19 +0000

    Trying again to commit filters.py
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/typhon/trunk@10571 aaf1aab0-4228-0410-ad68-8dceda47f409

---
+++ typhon/datasets/filters.py
@@ -0,0 +1,38 @@
+"""Collection of classes related to filtering
+
+"""
+
+import abc
+import numpy
+
+class OutlierFilter(metaclass=abc.ABCMeta):
+    
+    @abc.abstractmethod
+    def filter_outliers(self, C):
+        ...
+
+class MEDMAD(OutlierFilter):
+    """Outlier filter based on Median Absolute Deviation
+
+    """
+
+    def __init__(self, cutoff):
+        self.cutoff = cutoff
+    
+    def filter_outliers(self, C):
+        cutoff = self.cutoff
+        if C.ndim == 3:
+            med = numpy.ma.median(
+                C.reshape(C.shape[0]*C.shape[1], C.shape[2]),
+                0)
+            mad = numpy.ma.median(
+                abs(C - med).reshape(C.shape[0]*C.shape[1], C.shape[2]),
+                0)
+        elif C.ndim < 3:
+            med = numpy.ma.median(C.reshape((-1,)))
+            mad = numpy.ma.median(abs(C - med).reshape((-1,)))
+        else:
+            raise ValueError("Cannot filter outliers on "
+                f"input with {C.ndim:d}>3 dimensions")
+        fracdev = ((C - med)/mad)
+        return abs(fracdev) > cutoff
