2067721c72b96a4ce2994253cc015827ed722ee7
simonpf <simonpf@chalmers.se>
2017-09-19 00:05:43 +0200

    Added first tests for interactive ARTS interface.

---
+++ typhon/tests/test_workspace.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from typhon.arts.workspace import Workspace
+
+class TestWorkspace(object):
+
+    def __init__(self):
+        self.ws = Workspace()
+
+    def test_index_transfer(self):
+        self.ws.IndexCreate("index_variable")
+        i = np.random.randint(0, 100)
+        self.ws.index_variable = i
+        assert self.ws.index_variable.value == i
+
+    def test_array_of_index_transfer(self):
+        self.ws.ArrayOfIndexCreate("array_of_index_variable")
+        i = [np.random.randint(0, 100) for j in range(10)]
+        self.ws.array_of_index_variable = i
+        assert self.ws.array_of_index_variable.value == i
+
+    def test_string_transfer(self):
+        self.ws.StringCreate("string_variable")
+        s = "some random string."
+        self.ws.string_variable = s
+        assert self.ws.string_variable.value == s
+
+    def test_vector_transfer(self):
+        self.ws.VectorCreate("vector_variable")
+        v = np.random.rand(10)
+        self.ws.vector_variable = v
+        assert all(self.ws.vector_variable.value == v)
+
+    def test_matrix_transfer(self):
+        self.ws.MatrixCreate("matrix_variable")
+        m = np.random.rand(10, 10)
+        self.ws.matrix_variable = m
+        assert all(self.ws.matrix_variable.value.ravel() == m.ravel())
+
+    def test_supergeneric_overload_resolution(self):
+        self.ws.ArrayOfIndexCreate("array_of_index")
+        self.ws.ArrayOfArrayOfIndexCreate("array_of_array_of_index")
+        self.ws.array_of_index = [1,2,3]
+        self.ws.Append(self.ws.array_of_array_of_index, self.ws.array_of_index)
+        self.ws.Append(self.ws.array_of_array_of_index, self.ws.array_of_index)
