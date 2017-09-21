d22d8023ca66d3570e81d73184c31c70830bb4ad
Lukas Kluft <lukas.kluft@gmail.com>
2017-09-21 09:06:48 +0200

    Improve tests related to the ARTS API.
    Skip the whole test case if the ARTS library is not found.
    
    Use a setUp method instead of __init__ to ensure a new WorkSpace for
    every test run.

--- typhon/tests/test_workspace.py
+++ typhon/tests/test_workspace.py
@@ -8,48 +8,42 @@ except:
     skip_arts_tests = True
 
 
-class TestWorkspace(object):
-
-    @unittest.skipIf(skip_arts_tests, 'ARTS library not available')
-    def __init__(self):
+@unittest.skipIf(skip_arts_tests, 'ARTS library not available')
+class TestWorkspace(unittest.TestCase):
+    def setUp(self):
+        """This ensures a new Workspace for every test."""
         self.ws = Workspace()
 
-    @unittest.skipIf(skip_arts_tests, 'ARTS library not available')
     def test_index_transfer(self):
         self.ws.IndexCreate("index_variable")
         i = np.random.randint(0, 100)
         self.ws.index_variable = i
         assert self.ws.index_variable.value == i
 
-    @unittest.skipIf(skip_arts_tests, 'ARTS library not available')
     def test_array_of_index_transfer(self):
         self.ws.ArrayOfIndexCreate("array_of_index_variable")
         i = [np.random.randint(0, 100) for j in range(10)]
         self.ws.array_of_index_variable = i
         assert self.ws.array_of_index_variable.value == i
 
-    @unittest.skipIf(skip_arts_tests, 'ARTS library not available')
     def test_string_transfer(self):
         self.ws.StringCreate("string_variable")
         s = "some random string."
         self.ws.string_variable = s
         assert self.ws.string_variable.value == s
 
-    @unittest.skipIf(skip_arts_tests, 'ARTS library not available')
     def test_vector_transfer(self):
         self.ws.VectorCreate("vector_variable")
         v = np.random.rand(10)
         self.ws.vector_variable = v
         assert all(self.ws.vector_variable.value == v)
 
-    @unittest.skipIf(skip_arts_tests, 'ARTS library not available')
     def test_matrix_transfer(self):
         self.ws.MatrixCreate("matrix_variable")
         m = np.random.rand(10, 10)
         self.ws.matrix_variable = m
         assert all(self.ws.matrix_variable.value.ravel() == m.ravel())
 
-    @unittest.skipIf(skip_arts_tests, 'ARTS library not available')
     def test_supergeneric_overload_resolution(self):
         self.ws.ArrayOfIndexCreate("array_of_index")
         self.ws.ArrayOfArrayOfIndexCreate("array_of_array_of_index")
@@ -57,7 +51,6 @@ class TestWorkspace(object):
         self.ws.Append(self.ws.array_of_array_of_index, self.ws.array_of_index)
         self.ws.Append(self.ws.array_of_array_of_index, self.ws.array_of_index)
 
-    @unittest.skipIf(skip_arts_tests, 'ARTS library not available')
     def test_creation(self):
         self.ws.ArrayOfIndexCreate("array_of_index")
         self.ws.ArrayOfIndexCreate("array_of_index")
