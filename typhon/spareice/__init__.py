9db02f4cfc8d69a2cd24c240e6da6704e001ff3d
JohnMrziglod <mrzo@gmx.de>
2017-10-18 14:08:25 +0200

    Delete old spareice module.

--- typhon/spareice/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""All SPARE-ICE related modules."""
-
-from typhon.spareice.retriever import *  # noqa
-from typhon.spareice.trainer import *  # noqa
-
-__all__ = [s for s in dir() if not s.startswith('_')]
