e5fd37d568f0634bdebf6eaf5fbae2d6b58567c4
JohnMrziglod <mrzo@gmx.de>
2017-11-11 12:00:15 +0100

    Add test for __contains__ function.

--- typhon/tests/spareice/test_datasets.py
+++ typhon/tests/spareice/test_datasets.py
@@ -9,6 +9,21 @@ class TestDataset:
 
     refdir = join(dirname(__file__), 'reference')
 
+    def test_contains(self):
+        refpattern = (
+            "{year}/{month}/{day}/"
+            "{hour}{minute}{second}-{end_hour}{end_minute}{end_second}.nc"
+        )
+        dataset_files = join(self.refdir, refpattern)
+        ds = Dataset(dataset_files)
+
+        assert (
+            datetime(2016, 1, 1) not in ds
+            and datetime(2017, 1, 1) in ds
+            and "2016-01-01 00:00:00" not in ds
+            and "2017-01-01 00:00:00" in ds
+        )
+
     def test_find_file(self):
         refpattern = (
             "{year}/{month}/{day}/"
@@ -37,34 +52,31 @@ class TestDataset:
         )
 
         files = [
-            [join(self.refdir, '2017/01/01/000000-060000.nc'), [
+            [join(self.refdir, '2017/01/01/000000-060000.nc'), (
                 datetime(2017, 1, 1, 0, 0),
-                datetime(2017, 1, 1, 6, 0)]], [
-             join(self.refdir, '2017/01/01/060000-120000.nc'), [
+                datetime(2017, 1, 1, 6, 0))], [
+             join(self.refdir, '2017/01/01/060000-120000.nc'), (
                  datetime(2017, 1, 1, 6, 0),
-                 datetime(2017, 1, 1, 12, 0)]], [
-             join(self.refdir, '2017/01/01/120000-180000.nc'), [
+                 datetime(2017, 1, 1, 12, 0))], [
+             join(self.refdir, '2017/01/01/120000-180000.nc'), (
                  datetime(2017, 1, 1, 12, 0),
-                 datetime(2017, 1, 1, 18, 0)]], [
-             join(self.refdir, '2017/01/01/180000-000000.nc'), [
+                 datetime(2017, 1, 1, 18, 0))], [
+             join(self.refdir, '2017/01/01/180000-000000.nc'), (
                  datetime(2017, 1, 1, 18, 0),
-                 datetime(2017, 1, 2, 0, 0)]], [
-             join(self.refdir, '2017/01/02/000000-060000.nc'), [
+                 datetime(2017, 1, 2, 0, 0))], [
+             join(self.refdir, '2017/01/02/000000-060000.nc'), (
                  datetime(2017, 1, 2, 0, 0),
-                 datetime(2017, 1, 2, 6, 0)]], [
-             join(self.refdir, '2017/01/02/060000-120000.nc'), [
+                 datetime(2017, 1, 2, 6, 0))], [
+             join(self.refdir, '2017/01/02/060000-120000.nc'), (
                  datetime(2017, 1, 2, 6, 0),
-                 datetime(2017, 1, 2, 12, 0)]], [
-             join(self.refdir, '2017/01/02/120000-180000.nc'), [
+                 datetime(2017, 1, 2, 12, 0))], [
+             join(self.refdir, '2017/01/02/120000-180000.nc'), (
                  datetime(2017, 1, 2, 12, 0),
-                 datetime(2017, 1, 2, 18, 0)]], [
-             join(self.refdir, '2017/01/02/180000-000000.nc'), [
+                 datetime(2017, 1, 2, 18, 0))], [
+             join(self.refdir, '2017/01/02/180000-000000.nc'), (
                  datetime(2017, 1, 2, 18, 0),
-                 datetime(2017, 1, 3, 0, 0)]]
+                 datetime(2017, 1, 3, 0, 0))]
         ]
-
-        print(files)
-        print(found_files)
 
         assert found_files == files
 
@@ -82,33 +94,30 @@ class TestDataset:
         )
 
         files = [
-            [join(self.refdir, '2017/01/01/000000-060000.nc'), [
+            [join(self.refdir, '2017/01/01/000000-060000.nc'), (
                 datetime(2017, 1, 1, 0, 0),
-                datetime(2017, 1, 1, 0, 0)]], [
-             join(self.refdir, '2017/01/01/060000-120000.nc'), [
+                datetime(2017, 1, 1, 0, 0))], [
+             join(self.refdir, '2017/01/01/060000-120000.nc'), (
                  datetime(2017, 1, 1, 6, 0),
-                 datetime(2017, 1, 1, 6, 0)]], [
-             join(self.refdir, '2017/01/01/120000-180000.nc'), [
+                 datetime(2017, 1, 1, 6, 0))], [
+             join(self.refdir, '2017/01/01/120000-180000.nc'), (
                  datetime(2017, 1, 1, 12, 0),
-                 datetime(2017, 1, 1, 12, 0)]], [
-             join(self.refdir, '2017/01/01/180000-000000.nc'), [
+                 datetime(2017, 1, 1, 12, 0))], [
+             join(self.refdir, '2017/01/01/180000-000000.nc'), (
                  datetime(2017, 1, 1, 18, 0),
-                 datetime(2017, 1, 1, 18, 0)]], [
-             join(self.refdir, '2017/01/02/000000-060000.nc'), [
+                 datetime(2017, 1, 1, 18, 0))], [
+             join(self.refdir, '2017/01/02/000000-060000.nc'), (
                  datetime(2017, 1, 2, 0, 0),
-                 datetime(2017, 1, 2, 0, 0)]], [
-             join(self.refdir, '2017/01/02/060000-120000.nc'), [
+                 datetime(2017, 1, 2, 0, 0))], [
+             join(self.refdir, '2017/01/02/060000-120000.nc'), (
                  datetime(2017, 1, 2, 6, 0),
-                 datetime(2017, 1, 2, 6, 0)]], [
-             join(self.refdir, '2017/01/02/120000-180000.nc'), [
+                 datetime(2017, 1, 2, 6, 0))], [
+             join(self.refdir, '2017/01/02/120000-180000.nc'), (
                  datetime(2017, 1, 2, 12, 0),
-                 datetime(2017, 1, 2, 12, 0)]], [
-             join(self.refdir, '2017/01/02/180000-000000.nc'), [
+                 datetime(2017, 1, 2, 12, 0))], [
+             join(self.refdir, '2017/01/02/180000-000000.nc'), (
                  datetime(2017, 1, 2, 18, 0),
-                 datetime(2017, 1, 2, 18, 0)]]
+                 datetime(2017, 1, 2, 18, 0))]
         ]
-
-        print(files)
-        print(found_files)
 
         assert found_files == files
