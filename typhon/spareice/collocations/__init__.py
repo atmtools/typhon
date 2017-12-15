83361d2e5cd1cbada31da7350c653133ae9a185f
John Mrziglod <mrzo@gmx.de>
2017-12-15 17:29:38 +0100

    Divide collocations package into submodules

---
+++ typhon/spareice/collocations/__init__.py
@@ -0,0 +1,16 @@
+"""
+This module contains classes to find collocations between datasets. They are
+inspired by the implemented CollocatedDataset classes in atmlab written by
+Gerrit Holl.
+
+TODO: I would like to have this package as typhon.collocations.
+
+Created by John Mrziglod, June 2017
+"""
+
+from .common import *  # noqa
+
+__all__ = [
+    "CollocatedDataset",
+    "NotCollapsedError",
+]
