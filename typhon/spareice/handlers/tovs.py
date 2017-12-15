6740396351751a82df87cf1c60f070e8daf1f487
John Mrziglod <mrzo@gmx.de>
2017-12-15 17:37:42 +0100

    Add scanline and pos, disable time interpolation

--- typhon/spareice/handlers/tovs.py
+++ typhon/spareice/handlers/tovs.py
@@ -21,13 +21,20 @@ class MHSAAPP(handlers.FileHandler):
     # This file handler always wants to return at least time, lat and lon
     # fields. These fields are required for this:
     standard_fields = [
-        "Data/scnlintime", "Data/scnlinyr", "Data/scnlindy",
-        "Geolocation/Latitude", "Geolocation/Longitude"]
+        "Data/scnlintime",  # milliseconds since midnight
+        "Data/scnlinyr",
+        "Data/scnlindy",
+        "Data/scnlin",
+        "Geolocation/Latitude",
+        "Geolocation/Longitude"
+    ]
 
     mapping = {
         "Geolocation/Latitude": "lat",
         "Geolocation/Longitude": "lon",
+        "Data/scnlin": "scnline",
     }
+    inv_mapping = {v: k for k, v in mapping.items()}
 
     def __init__(self, mapping=None, apply_scaling=True, **kwargs):
         """
@@ -68,18 +75,21 @@ class MHSAAPP(handlers.FileHandler):
         TODO: Extend documentation.
         """
 
-        if fields is not None:
-            fields_to_extract = fields + self.standard_fields
-            fields_to_extract = set(fields_to_extract)
-            try:
-                fields_to_extract.remove("time")
-                fields_to_extract.remove("lat")
-                fields_to_extract.remove("lon")
-            except KeyError:
-                pass
-            fields_to_extract = list(fields_to_extract)
+        if fields is None:
+            fields_to_extract = self.standard_fields
         else:
-            fields_to_extract = fields
+            mapped_fields = [
+                self.inv_mapping.get(field, field)
+                for field in fields
+            ]
+            fields_to_extract = mapped_fields + self.standard_fields
+            fields_to_extract = set(fields_to_extract)
+            for field in ["time", "scnpos"]:
+                try:
+                    fields_to_extract.remove(field)
+                except KeyError:
+                    pass
+            fields_to_extract = list(fields_to_extract)
 
         dataset = GeoData.from_netcdf(filename, fields_to_extract)
         dataset.name = "MHS"
@@ -95,22 +105,25 @@ class MHSAAPP(handlers.FileHandler):
         dataset["lon"] = dataset["lon"].flatten()
         dataset["lat"] = dataset["lat"].flatten()
 
+        # Repeat the scanline and create the scnpos:
+        dataset["scnpos"] = np.tile(np.arange(1, 91), dataset["scnline"].size)
+        dataset["scnline"] = np.repeat(dataset["scnline"], 90)
+
         # Some fields need special treatment
         vars_to_drop = []
         for var in dataset.vars(deep=True):
             # Some variables have been loaded only for temporary reasons.
             if (var in self.standard_fields
-                    and fields is not None
-                    and var not in fields):
+                    or (fields is not None and var not in fields)):
                 vars_to_drop.append(var)
 
-            if var == "Data/btemps":
-                # Unfold the dimension of the brightness temperature variable
+            # Unfold the variable automatically if it is a swath variable.
+            if len(dataset[var].shape) > 1 and dataset[var].shape[1] == 90:
+                # Unfold the dimension of the variable
                 # to the shapes of the time vector.
                 dataset[var] = dataset[var].reshape(
-                    dataset[var].shape[0] * 90, 5
+                    -1, dataset[var].shape[-1]
                 )
-                dataset[var].dims = ["time_id", "channel"]
 
             # Some variables are scaled. If the user wants us to do
             # rescaling, we do it and delete the note in the attributes.
@@ -124,39 +137,50 @@ class MHSAAPP(handlers.FileHandler):
             dataset.rename(self.user_mapping)
         return dataset
 
-    def _get_time_field(self, dataset):
-        # Interpolate between the start times of each swath to retrieve the
-        # timestamp of each pixel.
-        swath_start_indices = np.arange(
-            0, dataset["Data"]["scnlintime"].size * 90, 90)
-        pixel_times = np.interp(
-            np.arange((dataset["Data/scnlintime"].size - 1) * 90),
-            swath_start_indices, dataset["Data/scnlintime"])
-
-        # Add the timestamps from the last swath here, we could not interpolate
-        # them in the step before because we do not have an ending timestamp.
-        # We simply extrapolate from the difference between the last two
-        # timestamps.
-        last_swath_pixels = \
-            dataset["Data/scnlintime"][-1] \
-            - dataset["Data/scnlintime"][-2] \
-            + np.linspace(
-                dataset["Data/scnlintime"][-2], dataset["Data/scnlintime"][-1],
-                90, dtype="int32", endpoint=False)
-        pixel_times = np.concatenate([pixel_times, last_swath_pixels])
-
-        # Convert the pixel times into timedelta objects
-        pixel_times = pixel_times.astype("timedelta64[ms]")
-
-        # Convert the swath time variables (year, day of year, miliseconds
-        # since midnight) to numpy.datetime objects. These are the start times
-        # of each swath, we have to bring them together with the interpolated
-        # pixel times.
+    @staticmethod
+    def _get_time_field(dataset):
         swath_times = \
             dataset["Data/scnlinyr"].astype('datetime64[Y]') - 1970 \
-            + dataset["Data/scnlindy"].astype('timedelta64[D]') - 1
+            + dataset["Data/scnlindy"].astype('timedelta64[D]') - 1 \
+            + dataset["Data/scnlintime"].astype("timedelta64[ms]")
 
-        swath_times = np.repeat(swath_times, 90)
-        times = swath_times + pixel_times
-
-        return times.flatten().astype("O")
+        return np.repeat(swath_times, 90).flatten()
+        # swath_times = \
+        #
+        #     dataset["Data/scnlintime"].astype("timedelta64[ms]")
+        #
+        # # Interpolate between the start times of each swath to retrieve the
+        # # timestamp of each pixel.
+        # swath_start_indices = np.arange(
+        #     0, dataset["Data/scnlintime"].size * 90, 90)
+        # pixel_times = np.interp(
+        #     np.arange((dataset["Data/scnlintime"].size - 1) * 90),
+        #     swath_start_indices, dataset["Data/scnlintime"])
+        #
+        # # Add the timestamps from the last swath here, we could not interpolate
+        # # them in the step before because we do not have an ending timestamp.
+        # # We simply extrapolate from the difference between the last two
+        # # timestamps.
+        # last_swath_pixels = \
+        #     dataset["Data/scnlintime"][-1] \
+        #     - dataset["Data/scnlintime"][-2] \
+        #     + np.linspace(
+        #         dataset["Data/scnlintime"][-2], dataset["Data/scnlintime"][-1],
+        #         90, dtype="int32", endpoint=False)
+        # pixel_times = np.concatenate([pixel_times, last_swath_pixels])
+        #
+        # # Convert the pixel times into timedelta objects
+        # pixel_times = pixel_times.astype("timedelta64[ms]")
+        #
+        # # Convert the swath time variables (year, day of year, miliseconds
+        # # since midnight) to numpy.datetime objects. These are the start times
+        # # of each swath, we have to bring them together with the interpolated
+        # # pixel times.
+        # swath_times = \
+        #     dataset["Data/scnlinyr"].astype('datetime64[Y]') - 1970 \
+        #     + dataset["Data/scnlindy"].astype('timedelta64[D]') - 1
+        #
+        # swath_times = np.repeat(swath_times, 90)
+        # times = swath_times + pixel_times
+        #
+        # return times.flatten()
