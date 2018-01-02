8158d4d88c81d753337c28d3946468bc43932c3a
JohnMrziglod <mrzo@gmx.de>
2018-01-02 17:39:25 +0100

    Fix various bugs from compression functions
    - decompress() works again
    - compress() creates zip files with proper filenames
     (not weird temporary file names)

--- typhon/files/utils.py
+++ typhon/files/utils.py
@@ -9,9 +9,9 @@ from contextlib import contextmanager
 __all__ = ['compress', 'compress_as', 'decompress']
 
 _known_compressions = {
-    '.gz': gzip.GzipFile,
-    '.bz2': bz2.BZ2File,
-    '.zip': zipfile.ZipFile,
+    'gz': gzip.GzipFile,
+    'bz2': bz2.BZ2File,
+    'zip': zipfile.ZipFile,
 }
 
 try:
@@ -23,7 +23,7 @@ else:
 
 
 @contextmanager
-def compress(filename, fmt=None, tmpdir=None):
+def compress(filename, fmt=None,):
     """Compress a file after writing to it.
 
     Supported compression formats are: gzip, bzip2, zip, and lzma (Python
@@ -42,37 +42,33 @@ def compress(filename, fmt=None, tmpdir=None):
         filename: The path and name of the compressed that should be
             created. The filename's extension decides to which format the
             file will be compressed (look at *fmt* for a list).
-        fmt: (optional) If want to specify the compression format without
-            the filename's extension, use this argument.
+        fmt: If want to specify the compression format without using the
+            filename's extension, use this argument.
             * *zip*: Uses the standard zip library.
             * *bz2*: Uses the bz2 library.
             * *gz*: Uses the GNU zip library.
             * *xz*: Uses the lzma format.
-        tmpdir (str): Path to directory for temporary storage of the
-            uncompressed file. The directory must exist. The default is the
-            temporary dir of the system.
 
     Yields:
         Generator containing the path to the temporary file.
 
     Examples:
-        >>> with typhon.files.compress('datafile.txt.gz') as file:
+        >>> with typhon.files.compress('datafile.nc.gz') as file:
         >>>     f = netCDF4.Dataset(file, 'w')
         >>>     #...
     """
+    filebase, fileext = os.path.splitext(filename)
+
     if fmt is None:
-        _, fmt = os.path.splitext(filename)
+        fmt = fileext.lstrip(".")
 
     if not is_compression_format(fmt):
         yield filename
         return
 
-    if tmpdir is None:
-        tmpdir = tempfile.gettempdir()
-
-    with tempfile.NamedTemporaryFile(prefix=os.path.join(tmpdir, '')) as tfile:
+    with tempfile.NamedTemporaryFile(prefix=tempfile.gettempdir()) as tfile:
         yield tfile.name
-        compress_as(tfile.name, fmt, filename)
+        compress_as(tfile.name, fmt, filename, True)
 
 
 def compress_as(filename, fmt, target=None, keep=True):
@@ -88,29 +84,35 @@ def compress_as(filename, fmt, target=None, keep=True):
             * *bz2*: Uses the bz2 library.
             * *gz*: Uses the GNU zip library.
             * *xz*: Uses the lzma format.
-        target: (optional) The default output filename is *filename.fmt*.
+        target: The default output filename is *filename.fmt*.
             If you do not like it, you can set another filename here.
-        keep: (optional) If true, keep the original file after
-            compressing. Otherwise it will be deleted. Default is keeping.
+        keep: If true, keep the original file after compressing. Otherwise it
+            will be deleted. Default is keeping.
 
     Returns:
         The filename of the newly created file.
     """
-    if not is_compression_format(fmt):
-        raise ValueError("Unknown compression format '%s'!" % suffix)
 
-    suffix = "."+fmt
+    if not is_compression_format(fmt):
+        raise ValueError("Unknown compression format '%s'!" % fmt)
 
     if target is None:
-        target = filename+fmt
+        target = ".".join([filename, fmt])
+
+    # The filename inside the zipped archive
+    target_filename = os.path.basename(target)
+    target_filename = target_filename.strip("."+fmt)
 
     # Read datafile in 100 MiB chunks for good performance/memory usage
     chunksize = 100 * 1024 * 1024
-    compfile = _known_compressions[suffix]
+    compfile = get_compressor(fmt)
     try:
-        if suffix == ".zip":
+        if fmt == "zip":
             with compfile(target, 'w') as f_out:
-                f_out.write(filename, compress_type=zipfile.ZIP_DEFLATED)
+                f_out.write(
+                    filename, arcname=target_filename,
+                    compress_type=zipfile.ZIP_DEFLATED
+                )
         else:
             with open(filename, 'rb') as f_in:
                 if fmt == "gz":
@@ -163,15 +165,16 @@ def decompress(filename, tmpdir=None):
 
     filebase, fileext = os.path.splitext(filename)
     filebase = os.path.basename(filebase)
+    fmt = fileext.lstrip(".")
 
-    if is_compression_format(fileext):
+    if is_compression_format(fmt):
         tmpfile = tempfile.NamedTemporaryFile(prefix=os.path.join(tmpdir, ''),
                                               delete=False)
         # Read datafile in 100 MiB chunks for good performance/memory usage
         chunksize = 100 * 1024 * 1024
-        compfile = _known_compressions[fileext]
+        compfile = get_compressor(fmt)
         try:
-            if fileext == '.zip':
+            if fmt == 'zip':
                 shutil.copyfileobj(compfile(filename, 'r').open(filebase, 'r'),
                                    tmpfile,
                                    chunksize)
@@ -188,8 +191,9 @@ def decompress(filename, tmpdir=None):
         yield filename
 
 
-def is_compression_format(fmt):
-    if not fmt.startswith("."):
-        fmt = "."+fmt
+def get_compressor(fmt):
+    return _known_compressions[fmt]
 
+
+def is_compression_format(fmt):
     return fmt in _known_compressions
