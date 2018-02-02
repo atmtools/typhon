6ac462900348a9c4af925bccf710f5ef67401887
John Mrziglod <mrzo@gmx.de>
2018-02-02 21:02:10 +0100

    Fix imports

--- typhon/spareice/__init__.py
+++ typhon/spareice/__init__.py
@@ -2,10 +2,8 @@
 
 """All SPARE-ICE related modules."""
 
-#from typhon.spareice.collocations import *  # noqa
+from typhon.spareice.collocations import *  # noqa
+from typhon.spareice.common import *  # noqa
 from typhon.spareice.datasets import *  # noqa
-#from typhon.spareice.geographical import *  # noqa
-from typhon.spareice.retriever import *  # noqa
-from typhon.spareice.trainer import *  # noqa
 
 __all__ = [s for s in dir() if not s.startswith('_')]
