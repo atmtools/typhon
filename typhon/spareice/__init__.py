ecd6378132d7de9756441dfe29bf6da446defd46
JohnMrziglod <mrzo@gmx.de>
2018-02-12 11:34:59 +0100

    Add handler imports

--- typhon/spareice/__init__.py
+++ typhon/spareice/__init__.py
@@ -6,5 +6,6 @@ from typhon.spareice.array import *
 from typhon.spareice.collocations import *  # noqa
 from typhon.spareice.common import *  # noqa
 from typhon.spareice.datasets import *  # noqa
+from typhon.spareice.handlers import FileHandler, FileInfo
 
 __all__ = [s for s in dir() if not s.startswith('_')]
