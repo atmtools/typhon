b761ce959f512ce391a2a073462d00e9c90ab1f3
Lukas Kluft <lukas.kluft@gmail.com>
2018-03-29 09:10:20 +0200

    Migrate phase example to cartopy.

--- doc/pyplots/plot_phase.py
+++ doc/pyplots/plot_phase.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
+"""Plot to demonstrate the phase colormap. """
 
-"""Plot to demonstrate the phase colormap.
-"""
-
-import numpy as np
 import matplotlib.pyplot as plt
-
-from netCDF4 import Dataset
-from mpl_toolkits.basemap import Basemap
+import netCDF4
+import numpy as np
+import cartopy.crs as ccrs
+from cartopy.mpl.gridliner import (LONGITUDE_FORMATTER, LATITUDE_FORMATTER)
 from matplotlib.ticker import FuncFormatter
 
-import typhon
+from typhon.plots.maps import get_cfeatures_at_scale
 
 
 @FuncFormatter
@@ -19,25 +17,41 @@ def degree_formatter(x, pos):
     return '{:.0f}\N{DEGREE SIGN}'.format(np.rad2deg(x))
 
 
-nc = Dataset('_data/test_data.nc')
-nth = 5
-lon, lat = np.meshgrid(nc.variables['lon'][::nth], nc.variables['lat'][::nth])
-u, v = nc.variables['u'][::nth, ::nth], nc.variables['v'][::nth, ::nth]
+# Read wind data.
+with netCDF4.Dataset('_data/test_data.nc') as nc:
+    nth = 5
+    lon, lat = np.meshgrid(nc.variables['lon'][::nth],
+                           nc.variables['lat'][::nth])
+    u, v = nc.variables['u'][::nth, ::nth], nc.variables['v'][::nth, ::nth]
 
 wdir = np.arctan2(u, v) + np.pi
 
+# Create plot with PlateCarree projection.
 fig, ax = plt.subplots(figsize=(10, 8))
-m = Basemap(projection='cyl', resolution='i',
-            llcrnrlat=47, llcrnrlon=3,
-            urcrnrlat=56, urcrnrlon=16)
-m.drawcoastlines()
-m.drawcountries()
-m.drawmeridians(np.arange(0, 20, 2), labels=[0, 0, 0, 1])
-m.drawparallels(np.arange(45, 60, 2), labels=[1, 0, 0, 0])
-m.quiver(lon, lat, u, v, wdir, cmap=plt.get_cmap('phase', 8), latlon=True)
-# Use our own ticklabel formatter.
-cb = m.colorbar(label='Wind direction', format=degree_formatter)
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.set_extent([3, 16, 47, 56])
+
+# Add map "features".
+features = get_cfeatures_at_scale(scale='50m')
+ax.add_feature(features.BORDERS)
+ax.add_feature(features.COASTLINE)
+ax.add_feature(features.OCEAN)
+
+# Plot the actual data.
+sm = ax.quiver(lon, lat, u, v, wdir,
+               cmap=plt.get_cmap('phase', 8),
+               transform=ccrs.PlateCarree(),
+               )
+
+# Add custom colorbar for wind directions (e.g. tick format).
+cb = fig.colorbar(sm, label='Wind direction', format=degree_formatter,
+                  fraction=0.0328, pad=0.02)
 cb.set_ticks(np.linspace(0, 2 * np.pi, 9))
+
+# Add coordinate system without drawing gridlines.
+gl = ax.gridlines(draw_labels=True, color='none')
+gl.xformatter, gl.yformatter = LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+gl.xlabels_top = gl.ylabels_right = False
 
 fig.tight_layout()
 plt.show()
