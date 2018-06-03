70904e563da70a095c15e32b342a4ac18db07d53
John Mrziglod <mrzo@gmx.de>
2018-06-03 17:05:00 +0200

    Update doc string

--- typhon/spareice/__init__.py
+++ typhon/spareice/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
-"""All SPARE-ICE related modules."""
+"""Implementation of SPARE-ICE
+
+This is a reimplementation of the toolkit developed by Gerit Holl for atmlab.
+
+TODO: Extend documentation and credits.
+"""
 
 __all__ = [s for s in dir() if not s.startswith('_')]
