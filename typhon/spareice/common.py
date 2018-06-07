a85f3bd4602898e3c3bcafa93d365f339cf2ce1a
John Mrziglod <mrzo@gmx.de>
2018-06-07 14:33:03 +0200

    Implement helper functions for inputs and targets

--- typhon/spareice/common.py
+++ typhon/spareice/common.py
@@ -21,8 +21,6 @@ Examples:
     data = spareice.prepare_training_data(
         # Do we have already collocations with all instruments? Put them here:
         collocations=...,
-        # and set collocate to False
-        collocate=False,
         # OR
         cloudsat=cloudsat, mhs=mhs, avhrr=avhrr,
         # Which time period should be used for training?
@@ -41,31 +39,37 @@ Examples:
     spareice.retrieve(
         # Do we have already collocations with MHS and AVHRR? Put them here:
         collocations=...,
-        collocate=False,
         # Otherwise we can put here each fileset and create collocations
         # on-the-fly
         mhs=mhs, avhrr=avhrr,
         # Which time period should be used for retrieving?
         start=..., end=...,
-
+        output=...,
     )
 """
 
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from typhon.collocations import Collocator
+from typhon.collocations import collapse, Collocator
 from typhon.retrieval import RetrievalProduct
+from typhon.utils.timeutils import to_datetime
 import xarray as xr
 
 
 class SPAREICE:
 
-    def __init__(self, file=None, verbose=1):
+    def __init__(self, file=None, collocator=None, verbose=1):
+        if collocator is None:
+            self.collocator = Collocator(
+                verbose=verbose,
+            )
+            self.collocator = collocator
         self.retrieval = RetrievalProduct(
             parameters_file=file,
-            #verbose=verbose
+            verbose=verbose
         )
+
         self.verbose = verbose
 
     def debug(self, msg):
@@ -83,131 +87,163 @@ class SPAREICE:
         self.retrieval.save_parameters(filename)
 
     @staticmethod
-    def get_training_collocations(
-        collocations, cloudsat, mhs, avhrr, start, end
+    def get_training_data(
+            collocations=None, cloudsat=None, mhs=None,
+            avhrr=None, start=None, end=None
     ):
-        if collocations is None:
-            # The user does not want to save the collocations to disk
-            ...
-        else:
-            # The user wants to save the collocations to disk
-            # Search for collocations and store them
-            collocations.search(
-                [mhs, cloudsat], start=start, end=end, processes=1,
-                max_interval="5 min", max_distance="7.5 km", verbose=2,
+
+        # We need all original data (cloudsat, mhs, avhrr) if we do not have
+        # collocations:
+        if (cloudsat is None or mhs is None or avhrr is None) \
+                and collocations is None:
+            raise ValueError(
+                "If no collocations are given, you need to pass the original "
+                "data of MHS and AVHRR!"
             )
 
-        return data
-
-    @staticmethod
-    def prepare_training_data(
-        collocations=None, cloudsat=None, mhs=None, avhrr=None,
-        start=None, end=None
-    ):
-
-        # We should examine this more deeply. What if `collocations`is given
-        # but empty?
-        collocations_found = collocations is not None
-
-        if collocations_found:
-            print("Found collocations for training data")
+        #
+        if cloudsat is None:
+            # The user has given some collocations to us, use them!
             data = xr.concat(collocations[start:end], dim="collocation")
         else:
             print("No collocations given or no collocations found in the given"
                   "time period. Start collocation toolkit to find them.")
-            data = SPAREICE.get_training_collocations(
-                collocations, cloudsat, mhs, avhrr, start, end
+            collocations.search(
+                [mhs, cloudsat], start=start, end=end, processes=1,
+                max_interval="5 min", max_distance="7.5 km", verbose=2,
             )
-
-        # Give more meaningful names to the variables
-        mapping = {
-            "Data_btemps": "MHS_BT",
-            "scnpos": "MHS_scnpos",
-            "MHS_2C-ICE/channel": "MHS_channel",
-            "Geolocation_Satellite_azimuth_angle": "satellite_azimuth_angle",
-            "Geolocation_Satellite_zenith_angle": "satellite_zenith_angle",
-            "Geolocation_Solar_azimuth_angle": "solar_azimuth_angle",
-            "Geolocation_Solar_zenith_angle": "solar_zenith_angle",
-            "AVHRR/Geolocation_Relative_azimuth_angle_mean":
-                "relative_azimuth_angle",
-            "2C-ICE_ice_water_path_mean": "IWP",
-            "2C-ICE_ice_water_path_std": "IWP_std",
-            "2C-ICE_ice_water_path_number": "IWP_number",
-            "AVHRR/Data_btemps_mean": "AVHRR_BT",
-            "AVHRR/Data_btemps_std": "AVHRR_std",
-            "AVHRR/Data_btemps_number": "AVHRR_number",
-            "AVHRR/scnpos_mean": "AVHRR_scnpos",
-            "AVHRR/channel": "AVHRR_channel",
-        }
-        data.rename(mapping, inplace=True)
+            ...
 
         return data
 
+    @staticmethod
+    def _get_inputs(data):
+        """Get the input fields for SPARE-ICE training / retrieval"""
+        inputs = pd.DataFrame({
+            "mhs_channel3": data["Data_btemps"].isel(
+                **{"MHS/channel": 2}
+            ),
+            "mhs_channel4": data["Data_btemps"].isel(
+                **{"MHS/channel": 3}
+            ),
+            "mhs_channel5": data["Data_btemps"].isel(
+                **{"MHS/channel": 4}
+            ),
+            "lat": data["lat"],
+            "mhs_scnpos": data["scnpos"],
+            "satellite_azimuth_angle":
+                data["Geolocation_Satellite_azimuth_angle"],
+            "satellite_zenith_angle":
+                data["Geolocation_Satellite_zenith_angle"],
+            "solar_azimuth_angle":
+                data["Geolocation_Solar_azimuth_angle"],
+            "solar_zenith_angle":
+                data["Geolocation_Solar_zenith_angle"],
+            "relative_azimuth_angle":
+                data["AVHRR/Geolocation_Relative_azimuth_angle_mean"],
+            "avhrr_channel1": data["AVHRR/Data_btemps_mean"].isel(
+                **{"AVHRR/channel": 1}
+            ),
+            "avhrr_channel2": data["AVHRR/Data_btemps_mean"].isel(
+                **{"AVHRR/channel": 2}
+            ),
+            "avhrr_channel3": data["AVHRR/Data_btemps_mean"].isel(
+                **{"AVHRR/channel": 3}
+            ),
+            "avhrr_channel4": data["AVHRR/Data_btemps_mean"].isel(
+                **{"AVHRR/channel": 4}
+            ),
+            "avhrr_channel5": data["AVHRR/Data_btemps_mean"].isel(
+                **{"AVHRR/channel": 5}
+            ),
+            "avhrr_scnpos": data["AVHRR/scnpos_mean"],
+        })
+
+        return inputs
+
+    @staticmethod
+    def _get_targets(data):
+        """Get the target fields for SPARE-ICE training"""
+        targets = pd.DataFrame({
+            "IWP_log10": np.log10(data["2C-ICE_ice_water_path_mean"])
+        })
+        return targets
+
     def train(self, data, test_ratio=None):
 
+        inputs = self._get_inputs(data)
+        targets = self._get_targets(data)
+
         # Make the training and testing data:
-        inputs = pd.DataFrame({
-            "mhs_channel3": data["MHS/Data_btemps"].isel(channel=2),
-            "mhs_channel4": data["MHS/Data_btemps"].isel(channel=3),
-            "mhs_channel5": data["MHS/Data_btemps"].isel(channel=4),
-            "lat": data["lat"],
-            "mhs_scnpos": data["MHS/scnpos"],
-        })
-        targets = pd.DataFrame({
-            "IWP_log10": np.log10(data["IWP"])
-        })
         inputs_train, inputs_test, targets_train, targets_test \
             = train_test_split(inputs, targets)
 
         print("Train SPARE-ICE")
         # If we have not trained it already, let's train it here:
-        train_score = retriever.train(
-            # The input fields for the training. The keys are the names of the input
-            # fields, the values define where the values for this input come from.
+        train_score = self.retrieval.train(
             inputs_train,
-            # The target fields for the training. The keys are the names of the
-            # target/output fields, the values define where the values for this target
-            # come from.
             targets_train,
-            verbose=2,
         )
-            print(f"Training score: {train_score:.2f}")
-        test_score = retriever.score(inputs_test, targets_test)
+        print(f"Training score: {train_score:.2f}")
+
+        test_score = self.retrieval.score(inputs_test, targets_test)
         print(f"Testing score: {test_score:.2f}")
 
-    def _get_retrieve_data(self, collocations, mhs, avhrr, output, start, end):
-        yield None
+    def _get_retrieval_data(self, collocations, collocate, mhs, avhrr,
+                            start, end):
+        if collocate:
+            data_iterator = self.collocator.collocate_filesets(
+                [avhrr, mhs], start=start, end=end, processes=10,
+                max_interval="5 min", max_distance="7.5 km",
+                verbose=self.verbose,
+            )
+            for data, attributes in data_iterator:
+                yield collapse(data), attributes
 
-    def _prepare_retrieve_inputs(self, data):
-        inputs = pd.DataFrame({
-            "mhs_channel3": data["MHS/Data_btemps"].isel(channel=2),
-            "mhs_channel4": data["MHS/Data_btemps"].isel(channel=3),
-            "mhs_channel5": data["MHS/Data_btemps"].isel(channel=4),
-            "lat": data["lat"],
-            "mhs_scnpos": data["MHS/scnpos"],
-        })
+                # Shall we save the collocations to disk?
+                if collocations is None:
+                    continue
 
-        return inputs
+                filename = collocations.get_filename(
+                    [to_datetime(data.attrs["start_time"]),
+                     to_datetime(data.attrs["end_time"])],
+                    fill=attributes
+                )
+
+                # Write the data to the file.
+                self.debug(f"Store collocations to \n{filename}")
+                collocations.write(data, filename)
+        elif collocations is None:
+            raise ValueError(
+                "You must pass a Collocations object via `collocations`."
+            )
+        else:
+            yield from collocations.icollect(start=start, end=end)
 
     def retrieve(
-            self, collocations=None, mhs=None, avhrr=None, output=None,
-            start=None, end=None
+            self, collocations=None, collocate=True, mhs=None, avhrr=None,
+            output=None, start=None, end=None,
     ):
 
-        data_iterator = self._get_retrieve_data(
-            collocations, mhs, avhrr, output, start, end
+        data_iterator = self._get_retrieval_data(
+            collocations, collocate, mhs, avhrr, start, end
         )
 
-        for data in data_iterator:
-            inputs = self._prepare_retrieve_inputs(data)
+        for data, attributes in data_iterator:
+            inputs = self._get_inputs(data)
 
-            retrieved = self.retrieval.run(inputs)
+            retrieved = self.retrieval.retrieve(inputs)
 
             # Add geolocation and time information:
             retrieved["lat"] = data["lat"]
             retrieved["lon"] = data["lon"]
             retrieved["time"] = data["time"]
 
-            # Store the output:
-            output[date1:date2] = retrieved
+            filename = output.get_filename(
+                [to_datetime(data.attrs["start_time"]),
+                 to_datetime(data.attrs["end_time"])], fill=attributes
+            )
 
+            # Write the data to the file.
+            self.debug(f"Store SPARE-ICE to \n{filename}")
+            output.write(retrieved, filename)
