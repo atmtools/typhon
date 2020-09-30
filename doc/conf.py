753631020d24130bda992a21298db42802d7002d
Oliver Lemke <olemke@gmail.com>
2020-09-30 15:23:14 +0200

    Fix import for in source tree build
    Module was imported before path adjustment. Led to error when trying to
    build the docs if typhon is not yet installed.

--- doc/conf.py
+++ doc/conf.py
@@ -16,12 +16,12 @@
 import sys
 import os
 
-from typhon.environment import environ
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+
+from typhon.environment import environ
 
 # -- General configuration ------------------------------------------------
 
