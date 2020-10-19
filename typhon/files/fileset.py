5f20dcfce1a5a7811a9b0350d27a95a01655b2f0
Gerrit Holl <gerrit.holl@dwd.de>
2020-10-19 17:00:09 +0200

    always / also when matching base dirs

--- typhon/files/fileset.py
+++ typhon/files/fileset.py
@@ -1287,10 +1287,10 @@ class FileSet:
             # some/all (?) file_system implementations do not end directories
             # in a /, glob.glob does
             if not (new_dir.endswith(os.sep) or new_dir.endswith("/")) and self.file_system.isdir(new_dir):
-                new_dir += os.sep
+                new_dir += "/"  # always / with AbstractFileSystem, not os.sep
             # The glob function yields full paths, but we want only to check
             # the new pattern that was added:
-            basename = new_dir[len(base_dir):].rstrip(os.sep)
+            basename = new_dir[len(base_dir):].rstrip(os.sep + "/")
             try:
                 new_attr = {
                     **dir_attr,
